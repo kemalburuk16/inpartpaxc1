bba5662055a3e0370fe83d2710512c2ec8a05d3a
kemalburuk16 <kemalburuk16@gmail.com>
2025-08-19 15:46:12 +0300

    Delete adminpanel directory

--- adminpanel/__init__.py
+++
@@ -1,7 +0,0 @@
-from .session_activity import session_activity_bp
-from .session_status import session_status_bp
-# Scheduler ve backup modülleri doğrudan import edilir
-
-def register_blueprints(app):
-    app.register_blueprint(session_activity_bp)
-    app.register_blueprint(session_status_bp)
--- adminpanel/session_backup.py
+++
@@ -1,9 +0,0 @@
-import shutil
-import time
-from app import SESSIONS_PATH
-
-def backup_sessions():
-    ts = time.strftime("%Y%m%d_%H%M%S")
-    backup_path = f"sessions_backup_{ts}.json"
-    shutil.copyfile(SESSIONS_PATH, backup_path)
-    return backup_path
